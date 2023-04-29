8e5f13efffc45c97031dd6ff805c3b28a605ce0f
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-29 12:41:46 -0500

    added an add to cart function to the product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Card, Button, Form, Row, Col } from 'react-bootstrap'
+import React, { useContext} from 'react'
+import { Card, Button } from 'react-bootstrap'
+import { CartContext } from '../CartContext'
+
 
 function ProductCard(props) {
   const product = props.product
+  const cart = useContext(CartContext)
+  const productQuantity = cart.getProductQuantity(product.id)
+  console.log(cart.items)
+
   return (
     <Card>
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
         <Card.Text>${product.price}</Card.Text>
-        <Button variant='primary'>Add To Cart</Button>
+        <Button variant='primary' onClick={() => cart.addOneToCart(product.id)} >Add To Cart</Button>
       </Card.Body>
     </Card>
   )
