4d05c5a7b109b1dfaa20c99b15f6a1c6dd018112
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-29 10:13:22 -0500

    added a nav bar component to the main app

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import { Button, Container, Navbar, Modal} from 'react-bootstrap'
 function NavbarComponent() {
   return (
-    <div className='nav-bar'>
-
-    </div>
+    <Navbar expand='sm'>
+      <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className='justify-content-end'>
+        <Button>Cart 0 Items</Button>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
