172a533612cf9dc15c1ad5b2ae85c2c3f261b8cf
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-01 13:03:28 -0500

    extracted out all of the bootstrap elements from the categories pages and style the raw elements

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import { Card, Button, Row } from 'react-bootstrap'
-import shirts from '../images/nimble-made-hMMXhKSZk7k-unsplash.jpg'
-import pants from '../images/lan-deng-quddu_dZKkQ-unsplash.jpg'
-import shoes from '../images/riyan-ong-j1PxAa2U-T4-unsplash.jpg'
+import './Categories.css'
 
 function Categories() {
   return (
-    <div>
-      <h1>Categories</h1>
-      <Row xs={1} md={3} className='g-4'>
+    <div className='category-page'>
+      <h3>Categories</h3>
+      <div className='category-display'>
+        <div className='category-card'>
+          <div id='shirts'></div>
+          <a href='/shirts'className='nav-link'>Shirts</a>
+        </div>
+        <div className='category-card'>
+          <div id='pants'></div>
+          <a href='/pants' className='nav-link'>Pants</a>
+        </div>
+        <div className='category-card'>
+          <div id='shoes'></div>
+          <a href='/shoes' className='nav-link'>Shoes</a>
+        </div>
+      </div>
+
+    {/*   <Row xs={1} md={3} className='g-4'>
         <Card bg='dark' variant='dark'>
           <Card.Body>
-            <Card.Img variant='top'style={{height: '500px', width: '300px', objectFit: 'cover',
-              backgroundPosition: 'center' }} src={shirts}></Card.Img>
+            <Card.Img variant='top'style={{}} src={shirts}></Card.Img>
             <Button sm="6" bg='dark' variant='dark' href='/shirts' className='mx-2'>Shirts</Button>
           </Card.Body>
         </Card>
@@ -30,7 +41,7 @@ function Categories() {
             <Button sm="6" bg='dark' variant='dark' href='/shoes' className='mx-2'>Shoes</Button>
           </Card.Body>
         </Card>
-      </Row>
+      </Row> */}
     </div>
   )
 }
