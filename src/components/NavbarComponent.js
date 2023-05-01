10250bf9964849a99d0f52448f9d3489c4ec1b7f
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-01 10:51:32 -0500

    extracting out some of the bootstrap components in order to style the page the way i would like

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,52 +1,17 @@
-import React, { useContext, useState } from 'react'
-import { Button, Navbar, Modal} from 'react-bootstrap'
+import React, { useContext} from 'react'
 import { CartContext } from '../CartContext'
-import CartProduct from './CartProduct'
 
 function NavbarComponent() {
   const cart = useContext(CartContext)
-
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
-
   const productsCount = cart.items.length
 
 
   return (
     <>
-      <Navbar  bg='dark' variant='dark'>
-        <a className="navLinks" href='/'>Fashion Apparel</a>
-        <Navbar.Toggle />
-        <Navbar.Collapse className='justify-content-end'>
-          <a className="navLinks" href='checkout'>{productsCount} items in cart</a>
-        </Navbar.Collapse>
-        
-      </Navbar>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title >Shopping Cart</Modal.Title>
-        </Modal.Header>
-        <Modal.Body >
-          {productsCount > 0 ?
-            <>
-              <p>Items in your cart:</p>
-              {cart.items.map((currentProduct, idx) => (
-                <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
-              ))}
-
-              <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
-
-              <Button variant='success'>
-                Purchase Items!
-              </Button>
-            </>
-          :
-          <h1>There Are No Items In Your Cart</h1>  
-          }
-        </Modal.Body>
-      </Modal>
-
+      <div className='header'>
+      <a className="navLinks" href='/'>Fashion Apparel</a>
+      <a className="navLinks" href='checkout'>{productsCount} items in cart</a>
+      </div>
     </>
   )
 }
