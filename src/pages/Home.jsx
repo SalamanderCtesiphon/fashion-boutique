fc8a1cde562bc0fdfde4bdae40fe79b073385cf1
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-09 11:42:36 -0500

    chaged out some images

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ObjectdeArt from '../images/Vector.svg'
+
+function Home() {
+  return (
+    <div className='home-page'>
+      <h1 className='title-header'>Electro Depot Computer Components</h1>
+      <img src={ObjectdeArt} alt='product' className='hero-img'></img>
+    </div>
+  )
+}
+
+export default Home
