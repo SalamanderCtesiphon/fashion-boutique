bf20cb06a4c3e0d8490bfc908f7f205aa6b92735
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-09 10:51:19 -0500

    began updating the style of the page

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react'
 import { CartContext } from '../CartContext'
 import Icon from '@mdi/react';
 import { mdiCart } from '@mdi/js';
-import search from '../search_FILL0_wght400_GRAD0_opsz48.svg'
 
 
 
@@ -22,15 +21,6 @@ function NavbarComponent() {
     <>
       <div className='header'>
       <a className="navLinks title" href='/'>Fashion Apparel</a>
-      <div id='search' >
-        <input placeholder='search site'></input>
-        <button onClick={() => toggleIsHidden()}>search</button>
-        <div className='drop-down' id='drop-down' style={style}>
-          <a className='list-item' href='/shoes'><img className="search-icon" src={search} alt='search icon'></img>Shoes</a>
-          <a className='list-item' href='/shirts'><img className="search-icon" src={search} alt='search icon'></img>Shirts</a>
-          <a className='list-item' href='/pants'><img className="search-icon" src={search} alt='search icon'></img>Pants</a>
-        </div>
-      </div>
       <div className='shopping-cart'>
         { productsCount > 0 ? 
           <div className='product-count'>{productsCount}</div>
