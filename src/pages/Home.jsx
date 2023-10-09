e5fafad060a891eff4f67230e7e7748651c69c0a
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-09 16:01:42 -0500

    added a link to the deparments page on the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,10 @@ import ObjectdeArt from '../images/Vector.svg'
 function Home() {
   return (
     <div className='home-page'>
-      <h1 className='title-header'>ElectroDepot Computer Components</h1>
+      <div className='title-header'>
+        <h1 >ElectroDepot Computer Components</h1>
+        <a href="/categories">View Component Departments</a>
+      </div>
       <img src={ObjectdeArt} alt='product' className='hero-img'></img>
     </div>
   )
