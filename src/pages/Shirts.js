75d27de41f68c4f0a5c753aaf968cc903cccdb92
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-09 15:53:32 -0500

    implementing features not bugs

--- src/pages/Shirts.js
+++ src/pages/Shirts.js
@@ -12,19 +12,9 @@ function Shirts() {
   return (
     <>
     <h1 align='center' className='p-3' >Welcome to the Shirt Department</h1>
-    <Row>
-      <Col><a href='/pants' className='navLinks' >Pants department</a></Col>
-      <Col><a href='/shoes' className='navLinks' >Shoe department</a></Col>
-    </Row>
-    <Row xs={1} md={3} className='g-4'>
-      {tempArray.map((product, idx) => (
-        <Col align="center" key={idx}>
-          <ProductCard product={product}></ProductCard>
-        </Col>
+    {tempArray.map((product, idx) => (
+        <ProductCard product={product}></ProductCard>
       ))}
-      
-
-    </Row>
   </>
   )
 }
