81d86cf8d4beaacbe436215705cf447717a2f009
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-09 14:59:27 -0500

    cleared out one final error msg

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { CartContext } from '../CartContext'
 import Icon from '@mdi/react';
 import { mdiCart } from '@mdi/js';
